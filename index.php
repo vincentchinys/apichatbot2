f243f7e51a105d211c5f893cb65370864dc45e9c
vince <35328797+vincentchinys@users.noreply.github.com>
2018-01-15 13:16:39 +0800

    Update index.php

--- index.php
+++ index.php
@@ -5,9 +5,27 @@
         $requestBody =  file_get_contents ('php://input');
         $json = json_decode($requestBody);
         $text = $json->result->pareneters->text;
-      
+         switch($text)   {
+            case 'hi':
+               $speech = "Hi, Nice to meet you";
+               break;
+
+            case 'bye' 
+               $speech = "Bye, good night"; 	
+               break;
+
+            case 'anything' 
+               $speech = "Yes, you can type anything here";
+               break;
+
+            default: 
+               $speech = "Sorry, I didn't get that. Please ask me something else."
+               break; 
+
+      }
      
         echo "test";
+   }
    else
    {
         echo "Method not allowed 12";
