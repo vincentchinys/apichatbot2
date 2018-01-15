0bee719336c1a69de398a807adc8969165547690
vince <35328797+vincentchinys@users.noreply.github.com>
2018-01-15 15:46:50 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,37 +1,31 @@
 <?php
-   $method = $_SERVER['REQUEST_METHOD'];
-   // Process only when method is POST 
-   If ($method == "POST"){ 
-        $requestBody =  file_get_contents ('php://input');
-        $json = json_decode($requestBody);
-        $text = $json->result->parameters->text;
-        
-      switch($text)   {
-            case 'hi':
-               $speech = "Hi, Nice to meet you";
-               break;
 
-           case 'bye':
-               $speech = "Bye, good night"; 	
-               break;
+$method = $_SERVER['REQUEST_METHOD'];
 
-         case 'anything':
-               $speech = "Yes, you can type anything here";
-               break;
-
-            default: 
-               $speech = "Sorry, I didn't get that. Please ask me something else.";
-               break; 
-
-      } 
-      $response = new \stdClass( ); 
-      $response->speech = "";
-      $response->displayText =  "";
-      $response->source = "webhook"; 
-      echo json_encode($response);
-   }
-   else
-   {
-        echo "Method not allowed 1";
-   }
-   ?>
+if($method == ""){
+	$requestBody = file_get_contents('php://input');
+	$json = json_decode(requestBody);
+	
+	$text = $json->result->parameters->text;
+	
+	switch ($text){
+		case 'hi':
+		$speech = "Hi Vincent";
+		break;
+		
+		default:
+		$speech = "Sorry Vincent";
+		break;
+		
+	}
+	
+	$response = new \stdClass();
+	$response->speech = "";
+	$response->displayText = "";
+	$response->source = "webhook";
+	echo json_decode($response);
+}
+else
+{
+	echo "Method not allowed";
+}
