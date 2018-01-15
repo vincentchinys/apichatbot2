dc59be98f55def550e7db72b74231330f3ff39a1
vince <35328797+vincentchinys@users.noreply.github.com>
2018-01-15 13:19:11 +0800

    Update index.php

--- index.php
+++ index.php
@@ -24,11 +24,14 @@
                break; 
 
       } 
-     
-        echo "test";
+      $response = new \stdClass( ); 
+      $response->speech = "";
+      $response->displayText =  "";
+      $response->source = "webhook"; 
+      echo json_encode($response);
    }
    else
    {
-        echo "Method not allowed 12";
+        echo "Method not allowed 1";
    }
    ?>
