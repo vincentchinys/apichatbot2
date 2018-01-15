b11b20de13b8a9873b5e617a68d3ac0db7d5cff6
vince <35328797+vincentchinys@users.noreply.github.com>
2018-01-15 13:08:04 +0800

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,9 @@
    $method = $_SERVER['REQUEST_METHOD'];
    // Process only when method is POST 
    If ($method == "POST"){ 
+        $requestBody =  file_get_contents ('php://input');
+        $json = json_decode($requestBody);
+        $text = $json->result->pareneters->text;
         echo "test";
    }
    else
